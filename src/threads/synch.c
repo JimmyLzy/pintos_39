8d0cceeb34b1adb4dde83aacb4abecfefde88e1a
yh6714 <yh6714@ic.ac.uk>
2016-02-08 18:39:45 +0000

    Modified sema_down to use list_push_back

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -63,8 +63,7 @@ void sema_down(struct semaphore *sema) {
 
     old_level = intr_disable();
     while (sema->value == 0) {
-        list_insert_ordered(&sema->waiters, &thread_current()->elem,
-                priority_compare, 0);
+        list_push_back(&sema->waiters, &thread_current()->elem);
         thread_block();
     }
     sema->value--;
